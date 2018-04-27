2713ffc5824785e12797b2e21b3933ab59a3635d
ProFiLeR4100 <profiler4100v2@gmail.com>
2018-04-27 13:45:07 +0300

    added VS Code preset

--- src/main.c
+++ src/main.c
@@ -3,9 +3,10 @@
 #include "mgos_rpc.h"
 #include "mgos_ir.h"
 
+const unsigned int LED = 2;
 
 static void led_timer_cb(void *arg) {
-  bool val = mgos_gpio_toggle(mgos_sys_config_get_pins_led());
+  bool val = mgos_gpio_toggle(LED);
   LOG(LL_INFO, ("%s uptime: %.2lf, RAM: %lu, %lu free", val ? "Tick" : "Tock",
                 mgos_uptime(), (unsigned long) mgos_get_heap_size(),
                 (unsigned long) mgos_get_free_heap_size()));
@@ -46,17 +47,17 @@ static void button_cb(int pin, void *arg) {
 }
 
 /* RPC callback for remote call of "IRRemoteControl" */
-static void http_over_ir_send(struct mg_rpc_request_info *ri,
-				  void *cb_arg, 
-				  struct mg_rpc_frame_info *fi,
-				  struct mg_str args) {
+// static void http_over_ir_send(struct mg_rpc_request_info *ri,
+// 				  void *cb_arg, 
+// 				  struct mg_rpc_frame_info *fi,
+// 				  struct mg_str args) {
 	
-}
+// }
 
 /* IR send procedure */
-static void ir_output () {
+// static void ir_output () {
 	
-}
+// }
 
 static void real_ir_input(int code, void *arg)
 {
@@ -66,25 +67,26 @@ static void real_ir_input(int code, void *arg)
 
 
 
-static void mgos_sys_reboot_handler() {
-  int delay_ms = 100;
-  json_scanf(args.p, args.len, ri->args_fmt, &delay_ms);
-  if (delay_ms < 0) {
-    mg_rpc_send_errorf(ri, 400, "invalid delay value");
-    ri = NULL;
-    return;
-  }
-  mgos_system_restart_after(delay_ms);
-  mg_rpc_send_responsef(ri, NULL);
-  ri = NULL;
-  (void) cb_arg;
-  (void) fi;
-}
+// static void mgos_sys_reboot_handler() {
+//   int delay_ms = 100;
+//   json_scanf(args.p, args.len, ri->args_fmt, &delay_ms);
+//   if (delay_ms < 0) {
+//     mg_rpc_send_errorf(ri, 400, "invalid delay value");
+//     ri = NULL;
+//     return;
+//   }
+//   mgos_system_restart_after(delay_ms);
+//   mg_rpc_send_responsef(ri, NULL);
+//   ri = NULL;
+//   (void) cb_arg;
+//   (void) fi;
+// }
 
 enum mgos_app_init_result mgos_app_init(void) {
   /* Blink built-in LED every second */
-  mgos_gpio_set_mode(mgos_sys_config_get_pins_led(), MGOS_GPIO_MODE_INPUT);
+  mgos_gpio_set_mode(LED, MGOS_GPIO_MODE_INPUT);
   mgos_set_timer(1000, MGOS_TIMER_REPEAT, led_timer_cb, NULL);
+
 
   /* Publish to MQTT on button press */
   mgos_gpio_set_button_handler(mgos_sys_config_get_pins_button(),
@@ -97,7 +99,7 @@ enum mgos_app_init_result mgos_app_init(void) {
   /* Remote HTTP call */
   // mgos_rpc_add_handler()
   // void *mgos_rpc_add_handler(const char *method, mgos_rpc_eh_t (*)(void *, char *, char *, cb_arg), cb_arg);
-  mgos_rpc_add_handler("IRRemoteControl" /* Command */, http_over_ir_send /* Callback */, NULL /* Params */);
+  // mgos_rpc_add_handler("IRRemoteControl" /* Command */, http_over_ir_send /* Callback */, NULL /* Params */);
   
   mgos_irrecv_nec_create(5 /* LED GPIO PIN */, real_ir_input /* Callback */, NULL);
  
